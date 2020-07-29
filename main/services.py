0b2d46bd880bbb6597f0482a3fdebe9c908da899
ratrevorrow <ratrevor@ncsu.edu>
2020-07-29 19:14:40 -0400

    added autopep8 formatter, removed site-packages

--- main/services.py
+++ main/services.py
@@ -15,37 +15,48 @@ DATE_FORMAT_2 = '%Y-%m-%d %H:%M:%S'
 time_slots = {}
 
 ####################### GET REQUESTS #######################
+
+
 def get_all_doctors():
     return __get_items(Doctor.objects.all())
+
 
 def get_appts_for_doc(pk):
     return __get_items(Appointment.objects.filter(doctor=pk))
 
+
 def get_appts():
     return __get_items(Appointment.objects.all())
+
 
 def get_times_available_for_date(pk, date):
     __init_time_slots()
     date = datetime.datetime.strptime(date, '%Y-%m-%d')
     items = __get_items(Appointment.objects.filter(date__date=date))
     for item in items:
-        date_time = datetime.datetime.strptime(item['fields']['date'], DATE_FORMAT_1)
+        date_time = datetime.datetime.strptime(
+            item['fields']['date'], DATE_FORMAT_1)
         time_slots[str(date_time.time())] -= 1
     return time_slots
 
 ####################### POST REQUESTS #######################
+
+
 def add_appt(new_appt):
-    new_appt['date'] = datetime.datetime.strptime(new_appt['date'], DATE_FORMAT_2)
+    new_appt['date'] = datetime.datetime.strptime(
+        new_appt['date'], DATE_FORMAT_2)
     new_appt['date'].astimezone(tz=pytz.UTC)
     return __create_or_fail(AppointmentSerializer(data=new_appt))
+
 
 def add_doc(doctor):
     return __create_or_fail(DoctorSerializer(data=doctor))
 
 ####################### DELETE REQUESTS #######################
+
+
 def delete_appt(pk):
     Appointment.objects.filter(id=pk).delete()
-
 
 
 ####################### HELPER FUNCTIONS #######################
@@ -56,8 +67,10 @@ def __create_or_fail(serializer):
         return True
     return False
 
+
 def __get_items(queryset):
     return json.loads(serializers.serialize("json", queryset))
+
 
 def __init_time_slots():
     for i in range(8, 10):
@@ -76,17 +89,22 @@ def __init_time_slots():
 
     time_slots['17:00:00'] = 3
 
+
 def is_fully_booked(new_appt):
-    length = len(__get_items(Appointment.objects.filter(doctor=new_appt['doctor'], date=new_appt['date'])))
+    length = len(__get_items(Appointment.objects.filter(
+        doctor=new_appt['doctor'], date=new_appt['date'])))
     if length >= 3:
         return True
     return False
 
+
 def is_patient_booked_already(new_appt):
-    length = len(__get_items(Appointment.objects.filter(date=new_appt['date'], first_name=new_appt['first_name'], last_name=new_appt['last_name'])))
+    length = len(__get_items(Appointment.objects.filter(
+        date=new_appt['date'], first_name=new_appt['first_name'], last_name=new_appt['last_name'])))
     if length > 0:
         return True
     return False
+
 
 def is_increment_of_fifteen(new_appt):
     date_time = new_appt['date'].split(' ')
